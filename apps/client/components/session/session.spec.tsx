89b224858ee6be28f4bf5a6f2f79ed51e75cd687
yehezkieldio <yehezkieldio35@gmail.com>
2022-07-07 09:11:12 +0800

    feat: session component

---
+++ apps/client/components/session/session.spec.tsx
@@ -0,0 +1,10 @@
+import { render } from "@testing-library/react";
+
+import Session from "./session";
+
+describe("Session", () => {
+    it("should render successfully", () => {
+        const { baseElement } = render(<Session />);
+        expect(baseElement).toBeTruthy();
+    });
+});
