00397e963646dc47334c2735cf1553801a4cf43e
yehezkieldio <yehezkieldio35@gmail.com>
2022-07-07 10:08:51 +0800

    enh: use try catch

--- apps/client/pages/api/auth/[...nextauth].ts
+++ apps/client/pages/api/auth/[...nextauth].ts
@@ -12,21 +12,25 @@ export default NextAuth({
                 password: { label: "Password", type: "password" },
             },
             async authorize(credentials, req) {
-                const response = await axios.post("http://localhost:3333/api/authorization/login", {
-                    username: credentials.username,
-                    password: credentials.password,
-                });
+                try {
+                    const response = await axios.post("http://localhost:3333/api/authorization/login", {
+                        username: credentials.username,
+                        password: credentials.password,
+                    });
 
-                const user = await axios.get("http://localhost:3333/api/users/me", {
-                    headers: {
-                        Authorization: `Bearer ${response.data.access_token}`,
-                    },
-                });
+                    const user = await axios.get("http://localhost:3333/api/users/me", {
+                        headers: {
+                            Authorization: `Bearer ${response.data.access_token}`,
+                        },
+                    });
 
-                if (user) {
-                    return user.data;
-                } else {
-                    return null;
+                    if (user) {
+                        return user.data;
+                    } else {
+                        return null;
+                    }
+                } catch (error) {
+                    console.error(error);
                 }
             },
         }),
